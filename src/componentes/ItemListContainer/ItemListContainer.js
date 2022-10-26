248edb7001c760cba8e9b271e7687aa43bcb2613
seba5 <sebastianrubilar27@gmail.com>
2022-10-26 20:35:01 -0300

    help me

--- src/componentes/ItemListContainer/ItemListContainer.js
+++ src/componentes/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,25 @@
+import React, { Fragment } from 'react'
+import ItemLista from '../Items/ItemList';
+
+
+
 function ItemList (props){
 return (
+  <Fragment>
 <div >
    <h1 className="titulo">{props.bienvenida}  </h1>
 <h2 className="body">{props.title} </h2>
-
 </div>
 
+<div >
+<ItemLista/> 
+</div>
+</Fragment>
 )
 
 }
+
+
+
+
 export default ItemList;
---
+++ src/componentes/Items/ItemList.js
@@ -0,0 +1,99 @@
+import { useState, useEffect } from "react";
+import Item from "./Items";
+import {useParams} from "react-router-dom";
+
+
+const ITEN = [
+ {
+     "id": "1",
+     "stock": "1",
+     "categoria":"clothes",
+     "nombre": " Camiseta Dark",
+     "valor": "$50 US",
+     "img": "",
+ },
+ {
+     "id": "2",
+     "stock": "1",
+     "categoria":"accesories",
+     "nombre": " Cartera Calavera",
+     "valor": "$20 US",
+     "img": "",
+ },
+ {
+     "id": "3",
+     "stock": "1",
+     "categoria":"shoes",
+     "nombre": " Botas Cuero",
+     "valor": "$150 US",
+     "img": "",
+ },
+ {
+     "id": "4",
+     "stock": "1",
+     "categoria":"shoes",
+     "nombre": " Botas Largas Negras",
+     "valor": "$250 US",
+     "img": "",
+ }
+]
+ function ItemLista(){
+
+  const [productos, setProductos] = useState();
+  const [loading, isLoading] = useState(false)
+  const {categoria} = useParams();
+
+
+  //SIMULACION API
+  const listado = () => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve(ITEN)
+            isLoading(true)
+        }, 3000);
+    })
+  }
+  useEffect(() => {
+    async function fetchedItems1(){
+      const items = await listado(); 
+      setProductos(items)
+    }
+
+    fetchedItems1()
+  }, [] );
+
+    return ( 
+        <div >
+          {!loading ? 
+        {
+            productos 
+            : categoria ? productos.filter((prod) => prod.categoria === categoria).map((el)=>(
+              <Item 
+             Id={el.id}
+              nombre={el.nombre}
+              stock={el.stock}
+              categoria={el.categoria}
+              img = {el.img}
+              valor = {el.valor}
+              />
+            ))
+  
+          : productos.map((el)=>(
+            <Item 
+            Id={el.id}
+              nombre={el.nombre}
+              stock={el.stock}
+              categoria={el.categoria}
+              img = {el.img}
+              valor = {el.valor}
+            />
+          ))
+          }
+          :
+         <p>Loading</p>
+        }
+        </div>
+
+    );
+} 
+export default ItemLista;
